2ec6f630c81ed0e6e3d98f56caeb7f9ae934fe53
loo-y <loo-y@outlook.com>
2024-01-03 22:34:17 +0800

    feat: fix mobile browser vh 2

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,7 +13,7 @@ export const viewport = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
     return (
         <html lang="en">
-            <body className={``}>{children}</body>
+            <body className={`main h-screen overflow-hidden`}>{children}</body>
         </html>
     )
 }
